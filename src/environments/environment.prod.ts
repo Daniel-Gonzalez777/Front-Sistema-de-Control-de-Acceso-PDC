70004c54d45f1f7dcc209fa399bc1b54be3b7644
Daniel-Gonzalez777 <danielgonzalezsuarez2006@gmail.com>
2026-08-27 10:21:39 -0500

    Se agrego la url de render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'sistema-de-control-de-acceso-back-pdc-production.up.railway.app'
+    apiUrl: 'https://sistema-de-control-de-acceso-back-pdc.onrender.com'
 };
